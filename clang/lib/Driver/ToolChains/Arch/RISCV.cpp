c70e043975fc5b24526c4fa3ae67a09516f8e9b6
b1f6c1c4 <b1f6c1c4@gmail.com>
2021-02-04 09:01:42 -0500

    Revert "Updated rvv support version from 0.9 to 1.0."
    This reverts commit 11f90dde27bf3c90ad856b88bb07b157e440c585.

--- clang/lib/Driver/ToolChains/Arch/RISCV.cpp
+++ clang/lib/Driver/ToolChains/Arch/RISCV.cpp
@@ -63,7 +63,7 @@ isExperimentalExtension(StringRef Ext) {
       Ext == "zbs" || Ext == "zbt" || Ext == "zbproposedc")
     return RISCVExtensionVersion{"0", "92"};
   if (Ext == "v")
-    return RISCVExtensionVersion{"1", "0"};
+    return RISCVExtensionVersion{"0", "9"};
   return None;
 }
 
